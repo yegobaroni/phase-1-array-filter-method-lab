0012d443e04e22a8e0fca7c97135fbd4f82147df
yego <yegokipkemoi254@gmail.com>
2022-08-24 22:49:42 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Code your solution here
+
+
+function findMatching(drivers, string) {
+    return drivers.filter(function (driver) { return driver.toLowerCase() === string.toLowerCase() })
+    }
+  you
+  swim
+  tomorrow
+  
+  function fuzzyMatch(drivers, string) {
+      return drivers.filter(function (driver) { return driver.toLowerCase().substring(0, string.length) === string.toLowerCase() })
+    }
+
+  function matchName(drivers, abdul) {
+      return drivers.filter(function (driver) { return driver.name === abdul })
+    }
